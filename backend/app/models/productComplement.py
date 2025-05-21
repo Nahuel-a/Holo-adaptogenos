71c84a87a1fd551c814a8587bebf2f5dee30aa2a
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-05-21 10:00:01 -0300

    refactor(models/productComplement): code refactoring and relationship implementation

--- backend/app/models/productComplement.py
+++ backend/app/models/productComplement.py
@@ -1,14 +1,28 @@
+from typing import TYPE_CHECKING, Optional
 from core.database import Base
-from sqlalchemy import String, Integer as Int
+from sqlalchemy import ForeignKey, Integer as Int
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from app.models.products import Product   # noqa: F401
+else:
+    Product = 'Product' # noqa: F401
+
 
 class ProductComplement(Base):
     __tablename__ = "product_complement"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    product_id: Mapped[int] = mapped_column(Int(255), nullable=False)#Foreign key to Product table
-    sub_product_id: Mapped[int] = mapped_column(Int(255), nullable=False)#Foreign key to Product table
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True,unique=True)
+    product_id: Mapped[Optional[int]] = mapped_column(ForeignKey('products.id'))
+    sub_product_id: Mapped[Optional[int]] = mapped_column(ForeignKey('products.id'))
     quantity_used: Mapped[float] = mapped_column(nullable=False)
 
     # Relationships
-    
+    product: Mapped[Product] = relationship(
+        back_populates="components", 
+        foreign_keys=[product_id]
+    )
+    sub_product: Mapped[Product] = relationship(
+        back_populates="used_in", 
+        foreign_keys=[sub_product_id]
+    )
