6317155f848702625ccdcad3a1e79a35c8de6e30
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-05-21 10:03:34 -0300

    refactor(models/purchasesProduct): code refactoring and relationship implementation

--- backend/app/models/purchasesProduct.py
+++ backend/app/models/purchasesProduct.py
@@ -1,16 +1,24 @@
+from typing import TYPE_CHECKING, List
 from core.database import Base
-from sqlalchemy import DateTime, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-
+if TYPE_CHECKING:
+    from app.models.purchases import Purchases  # noqa: F401
+    from app.models.products import Product  # noqa: F401
+else:
+    Purchases = 'Purchases' # noqa: F401
+    Product = 'Product' # noqa: F401
+    
 class PurchasesProducts(Base):
-    __tablename__ = "purchases_products"
+    __tablename__ = "table_purchases_products"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    #purchase_id: Mapped[int] = mapped_column(nullable=False)
-    #product_id: Mapped[int] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True,unique=True)
+    purchase_id: Mapped[int] = mapped_column(nullable=False)
+    product_id: Mapped[int] = mapped_column(nullable=False)
     quantity: Mapped[float] = mapped_column(nullable=False)
     unit_price: Mapped[float] = mapped_column(nullable=False)
+    sub_total: Mapped[float] = mapped_column(nullable=False)
 
     # Relationships
-    # purchases = relationship("Purchases", back_populates="purchases_products")
+    purchases: Mapped[Purchases] = relationship(back_populates="purchase_product")
+    products: Mapped[List[Product]] = relationship(back_populates="purchases_products")
