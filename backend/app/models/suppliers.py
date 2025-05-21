8346d2afb93f8cc44f3d3516c1fad2b8c0f91024
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-05-21 10:03:49 -0300

    refactor(models/suppliers): code refactoring and relationship implementation

--- backend/app/models/suppliers.py
+++ backend/app/models/suppliers.py
@@ -1,11 +1,11 @@
 from core.database import Base
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column 
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 
 class Suppliers(Base):
-    __tablename__ = "suppliers"
+    __tablename__ = "table_suppliers"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True,unique=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     phone_number: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
@@ -15,8 +15,10 @@ class Suppliers(Base):
 
     #def __repr__(self) -> str:
     
-        
-
+    purchases: Mapped[List[Purchase]] = relationship(
+        back_populates="supplier",
+        cascade="all, delete-orphan",
+    )
 
     
     
