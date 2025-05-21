f0bb8249c733bc1e254945bf3dfa9f796e3921b3
Nahuel-a <nahuel.94.arrieta@gmail.com>
2025-05-21 10:02:49 -0300

    refactor(models/purchases): code refactoring and relationship implementation

--- backend/app/models/purchases.py
+++ backend/app/models/purchases.py
@@ -1,18 +1,24 @@
+from typing import List
 from core.database import Base
-from sqlalchemy import DateTime, Enum
+from sqlalchemy import DateTime, Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Purchases(Base):
-    __tablename__ = "purchases"
+    __tablename__ = "table_purchases"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     purchase_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
-    supplier_id: Mapped[int] = mapped_column(nullable=False)
+    supplier_id: Mapped[int] = mapped_column(ForeignKey("suppliers.id"), nullable=False)
     payment_method: Mapped[Enum] = mapped_column(
         Enum('Chash', 'Transfer', 'Debit_card')
     )
-    total_amount: Mapped[float] = mapped_column(nullable=False)
+    total_price: Mapped[float] = mapped_column(nullable=False)
 
 
     # Relationships
-    # supplier = relationship("Supplier", back_populates="purchases")
+    supplier: Mapped[Suppliers] = relationship(back_populates="purchases")
+    purchases_products: Mapped[List[PurchasesProducts]] = relationship(
+        back_populates="purchases",
+        cascade="all, delete-orphan",
+        foreign_keys="PurchasesProducts.purchase_id",
+    )
